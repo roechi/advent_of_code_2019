b1125cb7012ec2e765c647fbe3711953b17b2201
Alexander Lüdeke & Richard Remus <alexander.luedeke+richard.remus@idealo.de>
2019-12-12 18:42:41 +0100

    refactors day 12

--- day_12/MoonTracker.py
+++ day_12/MoonTracker.py
@@ -1,28 +1,28 @@
+from functools import reduce
+
 import numpy as np
+from math import gcd
 
 
 class MoonTracker:
     def __init__(self, positions: [tuple]) -> None:
         self.bodies = list()
+        self.dimensions = len(positions[0])
         for p in positions:
-            self.bodies.append([np.array(p), np.array((0, 0, 0))])
-
-        #for b in self.bodies:
-         #   print(b)
+            self.bodies.append([np.array(p), np.array([0 for d in range(self.dimensions)])])
 
     def apply_step(self):
         for b in self.bodies:
             for o_b in self.bodies:
                 if not (o_b[0] == b[0]).all():
-                    for i in range(len(b[0])):
-                        if o_b[0][i] > b[0][i]:
-                            b[1][i] += 1
-                        elif o_b[0][i] < b[0][i]:
-                            b[1][i] -= 1
+                    for d in range(self.dimensions):
+                        if o_b[0][d] > b[0][d]:
+                            b[1][d] += 1
+                        elif o_b[0][d] < b[0][d]:
+                            b[1][d] -= 1
 
         for b in self.bodies:
             b[0] += b[1]
-            #print(b)
 
     def calc_system_energy(self) -> int:
         total_energy = 0
@@ -32,39 +32,29 @@ class MoonTracker:
         return total_energy
 
     def simulate_until_state_repeats(self) -> int:
+
+        seen_coords = [set() for d in range(self.dimensions)]
+        coords_have_repeated = [None for d in range(self.dimensions)]
+
         step = 0
-        repeated = False
 
-        states = list()
-        state = list()
-        for b in self.bodies:
-            state.append(np.concatenate(b))
+        while not all(v is not None for v in coords_have_repeated):
+            for d in range(self.dimensions):
+                if not coords_have_repeated[d]:
+                    d_state = list()
+                    for b in self.bodies:
+                        d_state.append(b[0][d])
+                        d_state.append(b[1][d])
+                    if str(d_state) in seen_coords[d]:
+                        print('Found dim {}: {}'.format(d, step))
+                        coords_have_repeated[d] = step
+                    seen_coords[d].add(str(d_state))
 
-        states.append(tuple(np.concatenate(state)))
-
-        while not repeated:
-            step += 1
-            if step % 1000 == 0:
-                print('Step: {}'.format(step))
             self.apply_step()
-            s = list()
-            for b in self.bodies:
-                s.append(np.concatenate(b))
+            step +=1
 
-            if tuple(np.concatenate(s)) in states:
-                return step
-            else:
-                states.append(tuple(np.concatenate(s)))
-
-        return step
+        return reduce(lambda step1, step2: MoonTracker.lcm(step1, step2), coords_have_repeated)
 
     @staticmethod
-    def states_equal(s1: [[np.array]], s2: [[np.array]]) -> bool:
-
-        for i in range(len(s1)):
-            if (s1[i][0] != s2[i][0]).any():
-                return False
-            if (s1[i][0] != s2[i][0]).any():
-                return False
-
-        return True
+    def lcm(x, y):
+        return x // gcd(x, y) * y
